d9ffa5cce4d6f22df086088928e75c0658a67f94
csfrancis94 <46684034+csfrancis94@users.noreply.github.com>
2019-03-26 15:51:35 -0400

    Created SiteUser model

--- esports_tracker_api/src/main/java/com/revature/models/SiteUser.java
+++ esports_tracker_api/src/main/java/com/revature/models/SiteUser.java
@@ -1,5 +1,21 @@
 package com.revature.models;
+import javax.persistence.Column;
 
+@Table(name="site_user")
+@Entity
 public class SiteUser {
 
+  @Id (name="site_user_id);
+  private int siteUserId;
+  
+  @Column
+  private String username;
+  
+  @Column(name="password_hash")
+  private String passwordHash;
+  
+  @Column
+  private String salt;
+  
+  
 }
