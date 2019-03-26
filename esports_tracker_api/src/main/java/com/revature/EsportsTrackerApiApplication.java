e74dca6a86c9c364cbd6ac2b91975e20abc67a12
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-03-26 15:35:59 -0400

    fixed api not connecting by fixing project foldr structure

---
+++ esports_tracker_api/src/main/java/com/revature/EsportsTrackerApiApplication.java
@@ -0,0 +1,19 @@
+package com.revature;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@Configuration
+@EnableSwagger2
+public class EsportsTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportsTrackerApiApplication.class, args);
+	}
+
+}
---
+++ esports_tracker_api/src/test/java/com/revature/esports_tracker/EsportsTrackerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.revature.esports_tracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EsportsTrackerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
