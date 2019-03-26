ed873b9e739c1e01da3b0091b01e6ea6c67e3488
csfrancis94 <csfrancis@email.com>
2019-03-26 16:11:06 -0400

    Small Push

--- esports_tracker_api/src/main/java/com/revature/models/SiteUser.java
+++ esports_tracker_api/src/main/java/com/revature/models/SiteUser.java
@@ -1,19 +1,30 @@
 package com.revature.models;
 import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Table(name="site_user")
 @Entity
 public class SiteUser {
 
-  @Id (name="site_user_id")
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="site_user_id")
   private int siteUserId;
   
+  @NotNull
   @Column
   private String username;
   
+  @NotNull
   @Column(name="password_hash")
   private String passwordHash;
   
+  @NotNull
   @Column
   private String salt;
   
--- esports_tracker_api/src/main/java/com/revature/models/Team.java
+++ esports_tracker_api/src/main/java/com/revature/models/Team.java
@@ -1,5 +1,23 @@
 package com.revature.models;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
+@Table(name="team")
+@Entity
 public class Team {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="team_id")
+  private int teamId;
+  
+  @NotNull
+  @Column(name="team_name")
+  private String teamName;
+  
 }
