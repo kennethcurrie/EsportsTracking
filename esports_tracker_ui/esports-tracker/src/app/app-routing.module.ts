e5c31696b9f4722a004533ad2b4f09a419972951
csfrancis94 <csfrancis@email.com>
2019-03-27 11:59:28 -0400

    "I added SomeComponent"

--- esports_tracker_ui/esports-tracker/src/app/app-routing.module.ts
+++ esports_tracker_ui/esports-tracker/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SomeComponentComponent } from './some-component/some-component.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, SomeComponentComponent],
+  declarations: [SomeComponentComponent]
 })
 export class AppRoutingModule { }
