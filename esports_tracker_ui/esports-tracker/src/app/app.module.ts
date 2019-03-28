bc93a67073d1bfdea74c8a6ed45bcb7db65988eb
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-03-28 10:46:55 -0400

    added stuff

--- esports_tracker_ui/esports-tracker/src/app/app.module.ts
+++ esports_tracker_ui/esports-tracker/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestViewComponent } from './test-view/test-view.component';
 import { SomeComponentComponent } from './some-component/some-component.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestViewComponent,
-    SomeComponentComponent
+    SomeComponentComponent,
+    PageWrapperComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -19,6 +23,6 @@ import { SomeComponentComponent } from './some-component/some-component.componen
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: []
+  exports: [PageWrapperComponent, HomeComponent]
 })
 export class AppModule { }
--- esports_tracker_ui/esports-tracker/src/environments/environment.ts
+++ esports_tracker_ui/esports-tracker/src/environments/environment.ts
@@ -22,7 +22,7 @@ const dev = {
   awsRegion:          'us-east-1',
   cognitoClientId:    '4mafdnad6u1o1ugokulsmbrl96',
   cognitoUserPoolId:  'us-east-1_6hCFlGZHZ',
-  smsContext:         'http://10.226.83.143:8092/',
+  smsContext:         'http://localhost:8092/',
   production: false
 };
 
