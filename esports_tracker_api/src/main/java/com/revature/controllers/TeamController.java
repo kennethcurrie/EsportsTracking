ca4f3a5b1e38f77c499f529c62c2078b8d7f00d9
kennethcurrie <kjcurrie@comcast.net>
2019-03-28 13:12:53 -0400

    Team work to API

---
+++ esports_tracker_api/src/main/java/com/revature/controllers/TeamController.java
@@ -0,0 +1,36 @@
+package com.revature.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.dto.LogNameData;
+import com.revature.models.Team;
+import com.revature.services.ITeamService;
+import com.revature.services.TeamService;
+
+@RestController
+@RequestMapping(path="team")
+public class TeamController {
+	
+	@Autowired
+	ITeamService teamService;
+
+	@GetMapping("")
+	public ResponseEntity<Team[]> findAll( ) {
+		List<Team> result = teamService.findAll();
+		Team[] arr = new Team[result.size()];
+		arr = result.toArray(arr);
+		return new ResponseEntity<Team[]>(arr, HttpStatus.OK);
+	}
+	
+}
+
+
---
+++ esports_tracker_api/src/main/java/com/revature/services/TeamService.java
@@ -0,0 +1,22 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.models.Team;
+import com.revature.repos.ITeamRepo;
+
+
+@Service
+public class TeamService implements ITeamService {
+
+	@Autowired
+	private ITeamRepo teamRepo;
+
+	@Override
+	public List<Team> findAll() {
+		return teamRepo.findAll();
+	}
+}
