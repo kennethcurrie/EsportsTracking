46ddce387623a063baa0f40365c806d6ad46822c
Peter Orlando <peter.emmanuel.orlando@gmail.com>
2019-03-28 09:13:48 -0400

    figured out binding, slight changes to api

--- esports_tracker_ui/esports-tracker/src/app/app.module.ts
+++ esports_tracker_ui/esports-tracker/src/app/app.module.ts
@@ -18,11 +18,7 @@ import { SomeComponentComponent } from './some-component/some-component.componen
     AppRoutingModule
   ],
   providers: [],
-<<<<<<< HEAD
   bootstrap: [AppComponent],
-=======
-  bootstrap: [AppComponent, TestViewComponent, SomeComponentComponent],
->>>>>>> 1ea2648eb7bcf6c9e2e4072d800df9a62924b591
   exports: []
 })
 export class AppModule { }
